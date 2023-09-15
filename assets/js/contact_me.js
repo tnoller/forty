c634e982d74fcc31cd6850e6c29f9bf1b6174236
tnoller <142343508+tnoller@users.noreply.github.com>
2023-09-15 16:33:33 -0400

    Form

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -35,7 +35,7 @@ $(function() {
       //   $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
   
       //   // debugging
-      //   console.log("FORM: " + name + ", " + phone + ", " + email + ", " + message);
+         console.log("FORM: ");
   
   
       //   $.ajax({
